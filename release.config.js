431bd0564ed6a90394450d69d251c62cc101ac1a
Rohmat <107597523+rohmatstar@users.noreply.github.com>
2025-07-02 14:11:40 +0700

    pkg: add bin folder to npm publishing

--- release.config.js
+++ release.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   branches: ["master"],
   plugins: [
-    "@semantic-release/commit-analyzer", // Baca commit message
-    "@semantic-release/release-notes-generator", // Buat release note
-    "@semantic-release/changelog", // Update CHANGELOG.md
-    "@semantic-release/npm", // Publish ke NPM
-    "@semantic-release/git", // Commit perubahan changelog + package.json
-    "@semantic-release/github", // Buat GitHub Release
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    "@semantic-release/git",
+    "@semantic-release/github",
   ],
   preset: "conventionalcommits",
 };
